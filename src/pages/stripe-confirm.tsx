66dbf0d225c951c06ca0a47fdc35412bcc493084
Marcin Baryłka <marcin.barylka@zageno.com>
2021-11-03 10:59:28 +0100

    feat: integration w/Stripe (wip)

---
+++ src/pages/stripe-confirm.tsx
@@ -0,0 +1,8 @@
+/*import Footer from '@/components/Footer';
+import NewNav from '@/components/NewNav';
+import NewsLetter from '@/components/promotion/NewsLetter';
+import Seo from '@/components/Seo';
+*/
+export default function StripeConfirmPage() {
+  return <></>;
+}
