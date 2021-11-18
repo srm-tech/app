07946ca939cb1d4c77a10665bd08e8ec962a95a6
Marcin Baryłka <marcin.barylka@zageno.com>
2021-11-18 15:01:27 +0100

    feat: drafts

--- src/models/Drafts.ts
+++ src/models/Drafts.ts
@@ -4,7 +4,7 @@ const { client, collection } = getDb('drafts');
 const Draft = {
   readMany: async (userId: ObjectId) => {
     await client.connect();
-    return collection.find({ _id: userId });
+    return collection.find({ _id: userId }).toArray();
   },
 };
 
