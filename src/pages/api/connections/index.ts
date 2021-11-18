ffdf1f1677b78a9b2f5818e59a3079dd7fa22881
Marcin Baryłka <marcin.barylka@zageno.com>
2021-11-18 17:38:28 +0100

    feat: apply easyGetAll()

--- src/pages/api/connections/index.ts
+++ src/pages/api/connections/index.ts
@@ -1,20 +1,11 @@
-// todo: replace UserId
-
 import { NextApiRequest, NextApiResponse } from 'next';
 
-import getCurrentUser from '@/lib/get-current-user';
-
 import Connection from '@/models/Connections';
+import { easyGetAll } from '@/lib/api-helpers';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  try {
-    const user = getCurrentUser();
-    const result = await Connection.readMany(user._id);
-    res.status(200).json(result);
-  } catch (err: any) {
-    res.status(500).json({ statusCode: 500, message: err.message });
-  }
+  easyGetAll(req, res, Connection);
 }
