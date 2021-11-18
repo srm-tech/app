9d3fb83400710da44ec78e79b4275cb6fe7d212e
Kris Zima <kris@mosquito.ie>
2021-11-18 22:32:38 +0800

    fix: add rewrite, env and useFetch for the frontend requests

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,35 +1,9 @@
 import { ClockIcon } from '@heroicons/react/solid';
 import * as React from 'react';
 import Link from 'next/link';
-import UnstyledLink from '../links/UnstyledLink';
-
-const scriptURL =
-  'https://hook.integromat.com/ndb1abbohl44oi2xsn23lhb7a1pvh3n5';
+import { SignUpForBetaForm } from './SignUpForBetaForm';
 
 export default function Hero() {
-  const [name, setName] = React.useState('');
-  const [contact, setContact] = React.useState('');
-  const formRef = React.useRef<any>();
-
-  const _handleSubmit = (e) => {
-    e.preventDefault();
-
-    const formdData = new FormData();
-    formdData.append('name', name);
-    formdData.append('contact', contact);
-    formdData.append('type', 'Beta Signup');
-
-    fetch(scriptURL, {
-      method: 'POST',
-      body: formdData,
-    }).then(() => {
-      alert('You have successfully submitted.');
-      setName('');
-      setContact('');
-    });
-    // .catch(error => )
-  };
-
   return (
     <section className='mt-16 mb-16 sm:mt-24'>
       <div className='mx-auto max-w-7xl'>
@@ -90,92 +64,7 @@ export default function Hero() {
             </div>
           </div>
           <div className='mx-5 mt-16 sm:mt-24 lg:mt-0 lg:col-span-6 justify-self-end'>
-            <div className='overflow-hidden bg-white rounded-lg sm:max-w-md sm:w-full'>
-              <div className='px-4 py-8 sm:px-10'>
-                <div>
-                  <p className='text-sm font-medium text-dark'>
-                    Sign up for beta
-                  </p>
-                </div>
-
-                <div className='mt-6'>
-                  <form
-                    ref={formRef}
-                    className='space-y-6'
-                    onSubmit={_handleSubmit}
-                  >
-                    <div>
-                      <label htmlFor='name' className='sr-only'>
-                        Full name
-                      </label>
-                      <input
-                        type='text'
-                        name='name'
-                        id='name'
-                        autoComplete='name'
-                        placeholder='Name'
-                        required
-                        className='block w-full border-gray-300 rounded-md shadow-sm focus:ring-green-500 focus:border-green-500 sm:text-sm'
-                        value={name}
-                        onChange={(event) => setName(event.target.value)}
-                      />
-                    </div>
-
-                    <div>
-                      <label htmlFor='mobile-or-email' className='sr-only'>
-                        Mobile number or email
-                      </label>
-                      <input
-                        type='text'
-                        name='mobile-or-email'
-                        id='mobile-or-email'
-                        autoComplete='email'
-                        placeholder='Mobile number or email'
-                        required
-                        className='block w-full border-gray-300 rounded-md shadow-sm focus:ring-green-500 focus:border-green-500 sm:text-sm'
-                        value={contact}
-                        onChange={(event) => setContact(event.target.value)}
-                      />
-                    </div>
-
-                    <div>
-                      <button
-                        type='submit'
-                        className='flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-primary-400 border border-transparent rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500'
-                      >
-                        Notify me
-                      </button>
-                    </div>
-                  </form>
-                </div>
-              </div>
-              <div className='px-4 py-6 border-t-2 border-gray-200 bg-gray-50 sm:px-10'>
-                <p className='text-xs leading-5 text-gray-500'>
-                  By signing up, you agree to our{' '}
-                  <UnstyledLink
-                    href='/legal/terms'
-                    className='font-medium text-gray-900 hover:underline'
-                  >
-                    Terms
-                  </UnstyledLink>
-                  ,{' '}
-                  <UnstyledLink
-                    href='/legal/privacy'
-                    className='font-medium text-gray-900 hover:underline'
-                  >
-                    Data Policy
-                  </UnstyledLink>{' '}
-                  and{' '}
-                  <UnstyledLink
-                    href='/legal/privacy'
-                    className='font-medium text-gray-900 hover:underline'
-                  >
-                    Cookies Policy
-                  </UnstyledLink>
-                  .
-                </p>
-              </div>
-            </div>
+            <SignUpForBetaForm />
           </div>
         </div>
       </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,34 @@
 import { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import { Provider } from 'use-http';
+import { env } from '../config';
 
 import '@/styles/globals.css';
 import '@/styles/roadmap.scss';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const options = {
+    interceptors: {
+      // every time we make an http request, this will run 1st before the request is made
+      // url, path and route are supplied to the interceptor
+      // request options can be modified and must be returned
+      // request: async ({ options, url, path, route }) => {
+      //   return options;
+      // },
+      // every time we make an http request, before getting the response back, this will run
+      response: async ({ response }) => {
+        const res = response;
+        return res;
+      },
+    },
+  };
+
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <Provider url={`${env.BASE_URL}/api`} options={options}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </Provider>
   );
 }
 
