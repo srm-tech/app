6c9e5406a84df96800010b3869655959a09fedc9
Kris Zima <kris@mosquito.ie>
2022-01-06 21:22:57 +0800

    fix: types

--- src/pages/api/business/[id]/defaultAgreement.ts
+++ src/pages/api/business/[id]/defaultAgreement.ts
@@ -6,6 +6,7 @@ import { check, validate } from '@/lib/validator';
 
 import getCollections from '@/models';
 import UserProfile from '@/models/UserProfiles';
+import { ObjectId } from 'mongodb';
 
 export const commissionLabel = {
   commissionPerReceivedLead: 'received introduction',
@@ -20,9 +21,10 @@ export default handleErrors(
     const { Agreement, UserProfile } = await getCollections();
     if (req.method === 'GET') {
       await validate([check('id').isMongoId()])(req, res);
-      const businessId = req.query.id.toString();
-      // result = await Agreement.findOne(businessId);
-      const business = await UserProfile.getOne(businessId);
+      const businessId = req.query.id;
+      const business = await UserProfile.getOne(
+        new ObjectId(businessId.toString())
+      );
       result = {
         commissionType: business?.commissionType,
         commissionValue: business?.[business?.commissionType],
